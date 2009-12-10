cdb202ad2ecec5070221882ca8919b2d34a4c4df
Rupert Swarbrick <rswarbrick@gmail.com>
2009-12-10 09:10:05 +0000

    Add dir-locals rule for C++ mode.
    In my emacs at least, .h files get opened in c++-mode.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ((nil . ((indent-tabs-mode . t)
          (tab-width . 4)
          (fill-column . 80)))
- (c-mode . ((c-basic-offset . 4))))
+ (c-mode . ((c-basic-offset . 4)))
+ (c++-mode . ((c-basic-offset . 4))))
